94cb6853927a914eaf9b564637fcd70a544cdbbe
FelipeQue <150635514+FelipeQue@users.noreply.github.com>
2024-01-21 17:05:04 -0300

    Add files via upload

--- M1S01/script.js
+++ M1S01/script.js
@@ -16,9 +16,12 @@ function changeBack(){
 
 // Ao clicar em um artigo, uma mensagem de alerta exibindo o título do projeto seja mostrada.
 
-let article1 = document.getElementById("mother");
-article1.addEventListener("click", function(){ alert("Mother of Waters."); });
+// let article1 = document.getElementById("mother");
+// article1.addEventListener("click", function(){ alert("Yes, this is a real project! It's a free game and it doesn't take long to play. Why not give it a go?"); });
 
-let article2 = document.getElementById("brasa");
-article2.addEventListener("click", function(){ alert("Brasa Game Assets."); });
+// let article2 = document.getElementById("brasa");
+// article2.addEventListener("click", function(){ alert("If you wanna know more about this, send me a message!"); });
+
+
+//external js: flickity.pkgd.js
 
